06c121215239fe64890faf2ba13bc1ef249beff8
I552\Administrator <aa>
2022-03-07 11:41:35 +0800

    add mockjs

--- Shopping-Admin-web/client/src/main.ts
+++ Shopping-Admin-web/client/src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueCompositionAPI, { createApp, h } from '@vue/composition-api'
 import { createPinia, PiniaVuePlugin } from 'pinia'
 import './index.css'
+import './mockjs/mock'
 
 import App from './App.vue'
 import router from './router'
---
+++ Shopping-Admin-web/client/src/mockjs/mock.ts
@@ -0,0 +1,26 @@
+// 导入mockjs
+import Mock from 'mockjs'
+// 获取 mock.Random 对象
+const Random = Mock.Random
+
+// mock新闻数据，包括新闻标题title、内容content、创建时间createdTime
+function produceNewsData() {
+  let newsList = []
+  for (let i = 0; i < 20; i++) {
+    let newNewsObject = {
+      //  Random.ctitle( min, max) 随机产生一个中文标题，长度默认在3-7之间
+      title: Random.ctitle(), 
+      // Random.cparagraph(min, max) 随机生成一个中文段落，段落里的句子个数默认3-7个
+      content: Random.cparagraph(),
+      // Random.date()指示生成的日期字符串的格式,默认为yyyy-MM-dd
+      createdTime: Random.date()
+    }
+    newsList.push(newNewsObject)
+  }
+  return newsList
+}
+
+// 拦截该url，就可以返回newsList
+Mock.mock('/mock/news', produceNewsData) 
+
+export default produceNewsData
