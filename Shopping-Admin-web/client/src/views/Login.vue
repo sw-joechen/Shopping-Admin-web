a23f94b059e6f4a6c9aad87ffe39038311ec3aa0
I552\Administrator <aa>
2022-03-07 13:23:01 +0800

    disable mockjs in prod mode

--- Shopping-Admin-web/client/src/views/Login.vue
+++ Shopping-Admin-web/client/src/views/Login.vue
@@ -6,17 +6,17 @@
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                         帳號
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" id="username" type="text" placeholder="帳號">
+                    <input v-model="account" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" id="username" type="text" placeholder="帳號">
                 </div>
                 <div class="mb-6">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="pwd">
                         密碼
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" id="pwd" type="password" placeholder="密碼">
+                    <input v-model="pwd" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" id="pwd" type="password" placeholder="密碼">
                     <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
                 </div>
                 <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" type="button">
+                    <button @click="loginHandler" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" type="button">
                         登入
                     </button>
                 </div>
@@ -27,12 +27,27 @@
 
 <script setup lang="ts">
 import { ref } from "@vue/composition-api";
+import axios from "axios";
+import { env } from "process";
 
 const account = ref()
 const pwd = ref()
 
-const loginHandler= async ()=>{
+const loginHandler = async ()=>{
+    console.log(import.meta.env.MODE)
+    const res = await axios({
+        method: 'post',
+        url: '/api/agent/loginAgent',
+        data: {
+            account: account.value,
+            pwd: pwd.value
+        }
+    })
+    console.log('res=>',res)
+    if (res.data) {
+        const data = JSON.parse(res.data)
 
+    }
 }
 
 </script>
