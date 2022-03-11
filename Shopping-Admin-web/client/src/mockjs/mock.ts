c6a64984bf0235d49f2d25702b91143dbab648cd
I552\Administrator <aa>
2022-03-11 17:27:46 +0800

    update mock

--- Shopping-Admin-web/client/src/mockjs/mock.ts
+++ Shopping-Admin-web/client/src/mockjs/mock.ts
@@ -5,10 +5,11 @@ const getAgentsList = () => {
     code: 200,
     msg: "success",
     data: [
-      { id: 0, account: "a12345", enabled: 0, createdDate: "2022/3/7 下午 03:41:06", updatedDate: "2022/3/7 下午 03:41:06", },
+      { id: 1, account: "a12345", enabled: 0, createdDate: "2022/3/7 下午 03:41:06", updatedDate: "2022/3/7 下午 03:41:06", },
       { id: 240, account: "a11111", enabled: 0, createdDate: "2022/3/7 下午 03:54:53", updatedDate: "2022/3/7 下午 03:54:53", },
       { id: 26, account: "a22222", enabled: 0, createdDate: "2022/3/7 下午 03:59:16", updatedDate: "2022/3/7 下午 03:59:16", },
-      { id: 13, account: "a33333", enabled: 1, createdDate: "2022/3/7 下午 04:40:51", updatedDate: "2022/3/7 下午 04:40:51", }
+      { id: 13, account: "a33333", enabled: 1, createdDate: "2022/3/7 下午 04:40:51", updatedDate: "2022/3/7 下午 04:40:51", },
+      { id: 97, account: "a33333", enabled: 1, createdDate: "2022/3/7 下午 04:40:51", updatedDate: "2022/3/7 下午 04:40:51", }
     ]
   }
   return JSON.stringify(result)
