ea734ce0a197a5abdd01e948fc6c5090c201faf5
I552\Administrator <aa>
2022-03-14 10:06:26 +0800

    add vue-i18n

--- Shopping-Admin-web/vue2Client/src/main.js
+++ Shopping-Admin-web/vue2Client/src/main.js
@@ -4,7 +4,9 @@ import router from "./router";
 import store from "./store";
 import "./index.css";
 import "./mockjs/mock";
+import VueI18n from "vue-i18n";
 
+Vue.use(VueI18n);
 Vue.config.productionTip = false;
 
 new Vue({
