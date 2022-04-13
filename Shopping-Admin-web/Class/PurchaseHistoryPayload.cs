e92119c6cd18ad89c18579cef7d80cd829a13b1b
snackboy <swjoechen@gmail.com>
2022-04-14 01:04:54 +0800

    [add] getMemberPurchaseHistory

---
+++ Shopping-Admin-web/Class/PurchaseHistoryPayload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Shopping_Admin_web.Class
+{
+    public class PurchaseHistoryPayload
+    {
+        // 可以自己塞初始值
+        // public int demo1 { get; set; } = -1;
+        // public bool demo2 { get; set; } = true;
+        public string account { get; set; }
+        public string startDate { get; set; }
+        public string dueDate { get; set; }
+    }
+}
---
+++ Shopping-Admin-web/Class/ShoppingItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Shopping_Admin_web.Class
+{
+    public class ShoppingItem
+    {
+        public int id { get; set; }
+        public int count { get; set; }
+    }
+}
