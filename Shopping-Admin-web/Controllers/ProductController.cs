e507968ed7cfede6a835bff86e0169887e0bba96
snackboy <swjoechen@gmail.com>
2022-04-19 15:09:50 +0800

    [add] Nlog in ProductController

--- Shopping-Admin-web/Controllers/ProductController.cs
+++ Shopping-Admin-web/Controllers/ProductController.cs
@@ -15,44 +15,45 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
-namespace Shopping_Admin_web.Controllers
-{
-    public class ProductController : ApiController
-    {
+namespace Shopping_Admin_web.Controllers {
+    public class ProductController : ApiController {
         string connectString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ConnDB"].ConnectionString;
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
         /// <summary>
         /// 新增商品
         /// </summary>
         [HttpPost]
         [Route("api/{controller}/addProduct")]
-        public string AddProduct()
-        {
+        public string AddProduct() {
             Result result = new Result(100, "缺少參數");
 
-            if (!Request.Content.IsMimeMultipartContent())
-            {
+            if (!Request.Content.IsMimeMultipartContent()) {
                 result.Set(110, "上傳格式錯誤");
+                Logger.Info($"result: {result.Stringify()}");
                 return result.Stringify();
             }
 
             var httpRequest = HttpContext.Current.Request;
 
             // 沒上傳圖檔
-            if (httpRequest.Files.Count < 1)
-            {
+            if (httpRequest.Files.Count < 1) {
                 result.Set(111, "未上傳檔案");
+                Logger.Info($"result: {result.Stringify()}");
                 return result.Stringify();
             }
 
-            if (httpRequest.Params["name"] == null || httpRequest.Params["price"] == null || httpRequest.Params["amount"] == null || httpRequest.Params["description"] == null || httpRequest.Params["type"] == null)
-            {
+            Logger.Info($"demo, name: {httpRequest.Params["name"]}, price: {httpRequest.Params["price"]}");
+            if (httpRequest.Params["name"] == null || httpRequest.Params["price"] == null ||
+                httpRequest.Params["amount"] == null || httpRequest.Params["description"] == null ||
+                httpRequest.Params["type"] == null
+            ) {
                 result.Set(100, "缺少參數");
+                Logger.Info($"result: {result.Stringify()}");
                 return result.Stringify();
             }
 
-            try
-            {
+            try {
                 string name = httpRequest.Params["name"];
                 string price = httpRequest.Params["price"];
                 string amount = httpRequest.Params["amount"];
@@ -60,38 +61,34 @@ namespace Shopping_Admin_web.Controllers
                 string type = httpRequest.Params["type"];
                 string picture;
 
-                Debug.WriteLine($"name=> {name}");
-                Debug.WriteLine($"price=> {price}");
-                Debug.WriteLine($"amount=> {amount}");
-                Debug.WriteLine($"description=> {description}");
-                Debug.WriteLine($"type=> {type}");
+                Logger.Info($"API: addProduct, name: {name}, price: {price}, amount: {amount}, description: {description}, type: {type}");
 
                 // 檢查價格不可為負數
-                if (Convert.ToInt32(price) < 0)
-                {
+                if (Convert.ToInt32(price) < 0) {
                     result.Set(114, "價格與數量不可為負");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
                 // 檢查數量不可為負數
-                if (Convert.ToInt32(amount) < 0)
-                {
+                if (Convert.ToInt32(amount) < 0) {
                     result.Set(114, "價格與數量不可為負");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
                 // 檢查名稱
                 SpecialCharacterValidator specialCharacterValidator = new SpecialCharacterValidator();
-                if (specialCharacterValidator.IsStrContainSpecialCharacter(name))
-                {
+                if (specialCharacterValidator.IsStrContainSpecialCharacter(name)) {
                     result.Set(113, "商品名稱不合法");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
                 // 檢查描述
-                if (specialCharacterValidator.IsStrContainSpecialCharacter(description))
-                {
+                if (specialCharacterValidator.IsStrContainSpecialCharacter(description)) {
                     result.Set(115, "商品描述不合法");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
@@ -104,12 +101,10 @@ namespace Shopping_Admin_web.Controllers
                 picture = $"/{DateTimeOffset.Now.ToUnixTimeSeconds()}_{postedFile.FileName}";
 
                 // 寫進庫
-                using (SqlConnection conn = new SqlConnection(connectString))
-                {
+                using (SqlConnection conn = new SqlConnection(connectString)) {
                     var sqlResponse = 0;
                     conn.Open();
-                    using (SqlCommand cmd = new SqlCommand("pro_saw_addProduct", conn))
-                    {
+                    using (SqlCommand cmd = new SqlCommand("pro_saw_addProduct", conn)) {
                         cmd.CommandType = CommandType.StoredProcedure;
                         cmd.Parameters.AddWithValue("@enabled", 1);
                         cmd.Parameters.AddWithValue("@name", name);
@@ -119,27 +114,24 @@ namespace Shopping_Admin_web.Controllers
                         cmd.Parameters.AddWithValue("@description", description);
                         cmd.Parameters.AddWithValue("@type", type);
                         SqlDataReader r = cmd.ExecuteReader();
-                        if (r.Read())
-                        {
+                        if (r.Read()) {
                             sqlResponse = (int)r["result"];
                         }
                     }
 
-                    if (sqlResponse == 200)
-                    {
+                    if (sqlResponse == 200) {
                         result.Set(200, "success");
                     }
-                    else
-                    {
+                    else {
                         result.Set(101, "網路錯誤");
                     }
                 }
             }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e);
-                return $"exception=> {e}";
+            catch (Exception ex) {
+                Logger.Error(ex);
+                result.Set(101, "網路錯誤");
             }
+            Logger.Info($"result: {result.Stringify()}");
             return result.Stringify();
         }
 
@@ -148,14 +140,12 @@ namespace Shopping_Admin_web.Controllers
         /// </summary>
         [HttpPost]
         [Route("api/{controller}/getProductsList")]
-        public string GetProductsList()
-        {
+        public string GetProductsList() {
             var httpRequest = HttpContext.Current.Request;
             Result result = new Result(100, "缺少參數");
             List<Product> productList = new List<Product> { };
 
-            try
-            {
+            try {
                 string paramID = httpRequest.Params["id"];
                 string name = httpRequest.Params["name"] ?? null;
                 string type = httpRequest.Params["type"] ?? null;
@@ -164,16 +154,11 @@ namespace Shopping_Admin_web.Controllers
                 int enabled = paramEnabled != null ? Convert.ToInt32(Convert.ToBoolean(paramEnabled)) : -1;
                 int id = paramID != null ? Convert.ToInt32(paramID) : -1;
 
-                Debug.WriteLine($"id=> {id}");
-                Debug.WriteLine($"name=> {name}");
-                Debug.WriteLine($"type=> {type}");
-                Debug.WriteLine($"enabled=> {enabled}");
+                Logger.Info($"API: getProductsList, id: {id}, name: {name}, type: {type}, enabled: {enabled}");
 
-                using (SqlConnection conn = new SqlConnection(connectString))
-                {
+                using (SqlConnection conn = new SqlConnection(connectString)) {
                     conn.Open();
-                    using (SqlCommand cmd = new SqlCommand("pro_saw_getProductList", conn))
-                    {
+                    using (SqlCommand cmd = new SqlCommand("pro_saw_getProductList", conn)) {
                         cmd.CommandType = CommandType.StoredProcedure;
                         if (id == -1)
                             cmd.Parameters.AddWithValue("@id", DBNull.Value);
@@ -199,12 +184,9 @@ namespace Shopping_Admin_web.Controllers
 
                         SqlDataReader r = cmd.ExecuteReader();
 
-                        if (r.HasRows)
-                        {
-                            while (r.Read())
-                            {
-                                productList.Add(new Product
-                                {
+                        if (r.HasRows) {
+                            while (r.Read()) {
+                                productList.Add(new Product {
                                     id = Convert.ToInt32(r["f_id"]),
                                     name = r["f_name"].ToString(),
                                     description = r["f_description"].ToString(),
@@ -218,18 +200,17 @@ namespace Shopping_Admin_web.Controllers
                                 });
                             }
                             result.Set(200, "success", productList);
+                            Logger.Info($"result: {result.Stringify()}");
                         }
-                        else
-                        {
+                        else {
                             result.Set(112, "找不到該商品");
+                            Logger.Info($"result: {result.Stringify()}");
                         }
                     }
-                    Debug.WriteLine($"productList=> {JsonConvert.SerializeObject(productList)}");
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"ex: {ex}");
+            catch (Exception ex) {
+                Logger.Error(ex);
                 result.Set(101, "網路錯誤");
             }
             return result.Stringify();
@@ -240,25 +221,23 @@ namespace Shopping_Admin_web.Controllers
         /// </summary>
         [HttpPost]
         [Route("api/{controller}/updateProduct")]
-        public string UpdateProduct()
-        {
+        public string UpdateProduct() {
             Result result = new Result(100, "缺少參數");
             var httpRequest = HttpContext.Current.Request;
 
-            if (!Request.Content.IsMimeMultipartContent())
-            {
+            if (!Request.Content.IsMimeMultipartContent()) {
                 result.Set(110, "上傳格式錯誤");
+                Logger.Info($"result: {result.Stringify()}");
                 return result.Stringify();
             }
 
-            if (httpRequest.Params["id"] == null || httpRequest.Params["type"] == null || httpRequest.Params["price"] == null || httpRequest.Params["amount"] == null || httpRequest.Params["name"] == null || httpRequest.Params["description"] == null)
-            {
+            if (httpRequest.Params["id"] == null || httpRequest.Params["type"] == null || httpRequest.Params["price"] == null || httpRequest.Params["amount"] == null || httpRequest.Params["name"] == null || httpRequest.Params["description"] == null) {
                 result.Set(100, "缺少參數");
+                Logger.Info($"result: {result.Stringify()}");
                 return result.Stringify();
             }
 
-            try
-            {
+            try {
                 int enabled = Convert.ToInt32(Convert.ToBoolean(httpRequest.Params["enabled"]));
                 int id = Convert.ToInt32(httpRequest.Params["id"]);
                 string name = httpRequest.Params["name"];
@@ -268,49 +247,40 @@ namespace Shopping_Admin_web.Controllers
                 string type = httpRequest.Params["type"];
                 string picture = $"/{Path.GetFileName(httpRequest.Params["picture"])}";
 
-                Debug.WriteLine($"id=> {id}");
-                Debug.WriteLine($"enabled=> {enabled}");
-                Debug.WriteLine($"name=> {name}");
-                Debug.WriteLine($"price=> {price}");
-                Debug.WriteLine($"amount=> {amount}");
-                Debug.WriteLine($"description=> {description}");
-                Debug.WriteLine($"type=> {type}");
-                Debug.WriteLine($"picture=> {picture}");
-
+                Logger.Info($"API: updateProduct, id: {id}, enabled: {enabled}, name: {name}, price: {price}, amount: {amount},description: {description}, type: {type}, picture: {picture}");
 
                 // 檢查價格不可為負數
-                if (Convert.ToInt32(price) < 0)
-                {
+                if (Convert.ToInt32(price) < 0) {
                     result.Set(114, "價格與數量不可為負");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
                 // 檢查數量不可為負數
-                if (Convert.ToInt32(amount) < 0)
-                {
+                if (Convert.ToInt32(amount) < 0) {
                     result.Set(114, "價格與數量不可為負");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
                 // 檢查名稱
                 SpecialCharacterValidator specialCharacterValidator = new SpecialCharacterValidator();
-                if (name != null && specialCharacterValidator.IsStrContainSpecialCharacter(name))
-                {
+                if (name != null && specialCharacterValidator.IsStrContainSpecialCharacter(name)) {
                     result.Set(113, "商品名稱不合法");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
                 // 檢查描述
-                if (description != null && specialCharacterValidator.IsStrContainSpecialCharacter(description))
-                {
+                if (description != null && specialCharacterValidator.IsStrContainSpecialCharacter(description)) {
                     result.Set(115, "商品描述不合法");
+                    Logger.Info($"result: {result.Stringify()}");
                     return result.Stringify();
                 }
 
-                if (httpRequest.Files.Count >= 1)
-                {
+                if (httpRequest.Files.Count >= 1) {
                     var postedFile = httpRequest.Files[0];
-                    Debug.WriteLine($"postedFile: {postedFile.FileName}");
+                    Logger.Info($"postedFile: {postedFile.FileName}");
 
                     // 串上時戳_原始檔名
                     var filePath = HttpContext.Current.Server.MapPath($"~/Uploads/{DateTimeOffset.Now.ToUnixTimeSeconds()}_{postedFile.FileName}");
@@ -320,11 +290,9 @@ namespace Shopping_Admin_web.Controllers
                 }
 
                 // 檢查完參數再寫進庫
-                using (SqlConnection conn = new SqlConnection(connectString))
-                {
+                using (SqlConnection conn = new SqlConnection(connectString)) {
                     conn.Open();
-                    using (SqlCommand cmd = new SqlCommand("pro_saw_editProduct", conn))
-                    {
+                    using (SqlCommand cmd = new SqlCommand("pro_saw_editProduct", conn)) {
                         cmd.CommandType = CommandType.StoredProcedure;
                         cmd.Parameters.AddWithValue("@id", id);
                         cmd.Parameters.AddWithValue("@enabled", enabled);
@@ -336,16 +304,14 @@ namespace Shopping_Admin_web.Controllers
                         cmd.Parameters.AddWithValue("@type", type);
                         SqlDataReader r = cmd.ExecuteReader();
 
-                        if (r.Read())
-                        {
+                        if (r.Read()) {
                             result.Set(200, "success");
                         }
                     }
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"ex: {ex}");
+            catch (Exception ex) {
+                Logger.Error(ex);
                 result.Set(101, "網路錯誤");
             }
             return result.Stringify();
@@ -356,49 +322,43 @@ namespace Shopping_Admin_web.Controllers
         /// </summary>
         [HttpPost]
         [Route("api/{controller}/delProduct")]
-        public string DelProduct()
-        {
+        public string DelProduct() {
             Result result = new Result(100, "缺少參數");
             var httpRequest = HttpContext.Current.Request;
 
-            if (!Request.Content.IsMimeMultipartContent())
-            {
+            if (!Request.Content.IsMimeMultipartContent()) {
                 result.Set(110, "上傳格式錯誤");
+                Logger.Info($"result: {result.Stringify()}");
                 return result.Stringify();
             }
 
-            if (httpRequest.Params["id"] == null)
-            {
+            if (httpRequest.Params["id"] == null) {
                 result.Set(100, "缺少參數");
+                Logger.Info($"result: {result.Stringify()}");
                 return result.Stringify();
             }
 
-            try
-            {
+            try {
                 int id = Convert.ToInt32(httpRequest.Params["id"]);
 
-                Debug.WriteLine($"id=> {id}");
+                Logger.Info($"API: delProduct, id: {id}");
 
                 // 檢查完參數再寫進庫
-                using (SqlConnection conn = new SqlConnection(connectString))
-                {
+                using (SqlConnection conn = new SqlConnection(connectString)) {
                     conn.Open();
-                    using (SqlCommand cmd = new SqlCommand("pro_saw_delProduct", conn))
-                    {
+                    using (SqlCommand cmd = new SqlCommand("pro_saw_delProduct", conn)) {
                         cmd.CommandType = CommandType.StoredProcedure;
                         cmd.Parameters.AddWithValue("@id", id);
                         SqlDataReader r = cmd.ExecuteReader();
 
-                        if (r.Read())
-                        {
+                        if (r.Read()) {
                             result.Set(Convert.ToInt32(r["result"]), "success");
                         }
                     }
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"ex: {ex}");
+            catch (Exception ex) {
+                Logger.Error(ex);
                 result.Set(101, "網路錯誤");
             }
             return result.Stringify();
--- Shopping-Admin-web/Global.asax.cs
+++ Shopping-Admin-web/Global.asax.cs
@@ -2,19 +2,16 @@
 using System.Web.Http;
 using System.Web.Routing;
 
-namespace Shopping_Admin_web
-{
-    public class Global : System.Web.HttpApplication
-    {
+namespace Shopping_Admin_web {
+    public class Global : System.Web.HttpApplication {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
-        protected void Application_Start(object sender, EventArgs e)
-        {
+        protected void Application_Start(object sender, EventArgs e) {
             // Add Routes.
             RegisterCustomRoutes(RouteTable.Routes);
         }
 
-        void RegisterCustomRoutes(RouteCollection routes)
-        {
+        void RegisterCustomRoutes(RouteCollection routes) {
             routes.MapPageRoute(
                 "index",
                 "*",
